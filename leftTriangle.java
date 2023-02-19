1b6f759cefd3dad25dccf114e40d6fec1cd2901c
abisaras <19ucs38@tcarts.in>
2023-02-19 22:39:31 +0530

    Practice

---
+++ leftTriangle.java
@@ -0,0 +1,9 @@
+package patterns;
+
+public class leftTriangle {
+
+}
+
+
+
+
---
+++ star.java
@@ -0,0 +1,18 @@
+package patterns;
+
+public class star {
+	public static void main (String args[])
+	{
+		int n=5,i,j;
+		for(i=1;i<=n;i++)
+		{
+			for(j=1;j<=n;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+	}
+
+}
+
+}
